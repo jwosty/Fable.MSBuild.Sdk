a0a71af7f075186a1f6e692d32d20f7c7032a48d
John Wostenberg <jwosty@users.noreply.github.com>
2023-10-17 08:09:40 -0600

    Make webpack bundle work in FableMinimal project

--- build_test_projects.sh
+++ build_test_projects.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-# PROJ=test_projects/FableMinimal/src/App.fsproj
-PROJ=test_projects/FableThreeProj/src/App/App.fsproj
+PROJ=test_projects/FableMinimal/src/App.fsproj
+# PROJ=test_projects/FableThreeProj/src/App/App.fsproj
 
 dotnet tool restore
 dotnet pack
